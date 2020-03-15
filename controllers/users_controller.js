1c84036ca06391bed2089b7b2a85c2ad46192c72
rahuljha661 <rahuljhamansapur@gmail.com>
2020-03-15 23:36:13 +0530

    5th commit

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,6 +1,52 @@
+const User=require('../models/users');
+
 module.exports.profile=function(req,res){
     // res.end('<h1>User Profile</h1>');
     res.render('user_profiles',{
         title:"UserProfile"
     });
+}
+//render the sign up pagee
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',{
+        title:"Codeial |SignUp"
+    });
+}
+//render the sign in page
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',{
+        title:"Codial | Sign In"
+    })
+}
+
+
+//get the sign up data
+
+module.exports.create=function(req,res){
+     if(req.body.password!=req.body.confirm_password){
+         return res.redirect('back');
+     }
+     User.findOne({email:req.body.email},function(err,user){
+         if(err){
+             console.log('error in finding user in signing up');
+             return;
+         }
+         if(!user){
+             User.create(req.body,function(err,user){
+                if(err){
+                    console.log('error in creating user while signing up');
+                    return;
+                }
+                return res.redirect('/users/sign-in');
+                        
+             })
+         }else{
+            return res.redirect('back');
+         }
+     });
+
+}
+//sign in and create the session for the user
+module.exports.createSession=function(req,res){
+   
 }
